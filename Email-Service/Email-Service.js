4c577f9d75c3160f21af07748328cb2606b9595f
Felix Matalik <fel.matalik.21@lehre.mosbach.dhbw.de>
2023-01-27 01:06:56 +0100

    Vollständig objektorientiert

--- Email-Service/Email-Service.js
+++ Email-Service/Email-Service.js
@@ -1,19 +1,15 @@
-const mqtt = require('async-mqtt');
-const mqttUrl = "mqtt://localhost";
 
-const mqttTopic1 = "alertTimeLimit";//->surpassed/under
-const mqttTopic2 = "alertTempLimit";//->surpassed/under
 
-let timeMessage = null;
-let tempMessage = null;
 /*Referenz für die Verwendung von Nodemailer: 
-https://www.w3schools.com/nodejs/nodejs_email.asp*/
+https://www.w3schools.com/nodejs/nodejs_email.asp
+MQTT inspiriert von Stefan Stumpf's Git*/
 
 class SendMail
 {
   constructor()
   {
     this.nodemailer = require('nodemailer');
+    this.config = new(require('../Configmanager/configmanager'))();
     this.transporter = this.nodemailer.createTransport({
       host: 'mail.gmx.net',
       port: 587,
@@ -25,33 +21,36 @@ class SendMail
       debug:true,
           auth: 
           {
-          user: 'smartfridge@gmx.net',
-          pass: 'SmarterKuhlschrank123'
+          user: this.config.get('mailSender:sender'),
+          pass: this.config.get('mailSender:mailPassword')
           }
     });
   }
 
-  //Mail versenden mit console-meldungen 
-  send(receiver, theme, content) 
-  {
-    //Wohin soll die Mail gehen mit welchem Inhalt:
-    let mailOptions = 
+    //Mail versenden mit console-meldungen 
+    send(receiver, theme, content) 
     {
-      from: 'smartfridge@gmx.net',
-      to: receiver,
-      subject: theme,
-      text: content
-    };
+        //Wohin soll die Mail gehen mit welchem Inhalt:
+        let mailOptions = 
+        {
+        from: this.config.get('mailSender:sender'),
+        to: receiver,
+        subject: theme,
+        text: content
+        };
 
-    this.transporter.sendMail(mailOptions, function(error, info)
-    {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log('Email sent: ' + info.response);
-      }
-    });
-  }
+        this.transporter.sendMail(mailOptions, function(error, info)
+        {
+        if (error) 
+        {
+            console.log(error);
+        } 
+        else 
+        {
+            console.log('Email sent: ' + info.response);
+        }
+        });
+    }
 }
 
 class EmailService extends SendMail
@@ -59,6 +58,7 @@ class EmailService extends SendMail
     constructor()
     {
         super();
+        this.config = new(require('../Configmanager/configmanager'))();
         this.theme = null;
         this.content = null; 
 
@@ -71,8 +71,9 @@ class EmailService extends SendMail
         if (timeMessage == "surpassed" && this.variableTime == 0)
         {
             this.theme = "Alarm: Ihr Kühlschrank ist zu lange geöffnet";
-            this.content = "Ihre Kühlschranktür ist länger, als die maximal zugelassene Öffnungszeit geöffnet. Bitte schließen Sie ihren Kühlschrank umgehend."
-            this.send("felix.matalik@gmail.com", this.theme, this.content);
+            this.content = "Ihre Kühlschranktür ist länger, als die maximal zugelassene Öffnungszeit von "
+            + this.config.get('alerts:timeLimitValue') + " Sekunden geöffnet. Bitte schließen Sie ihren Kühlschrank umgehend."
+            this.send(this.config.get('mailAdressRecipient'), this.theme, this.content);
             this.variableTime = 1;
         }
         else if (timeMessage == "under")
@@ -83,8 +84,9 @@ class EmailService extends SendMail
         if (tempMessage == "surpassed" && this.variableTemp == 0)
         {
             this.theme = "Alarm: Ihr Kühlschrank ist zu warm!"
-            this.content = "Die Temperatur im inneren Ihres Kühlschranks hat die eingestellte Höchsttemperatur überschritten";
-            this.send("felix.matalik@gmail.com", this.theme, this.content);
+            this.content = "Die Temperatur im inneren Ihres Kühlschranks hat die eingestellte Höchsttemperatur von "
+            + this.config.get('alerts:tempLimitValue') + " °C überschritten";
+            this.send(this.config.get('mailAdressRecipient'), this.theme, this.content);
             this.variableTemp = 1;
         }
         else if (tempMessage == "under")
@@ -95,53 +97,81 @@ class EmailService extends SendMail
     
 }
 
-const SendMailObject = new SendMail();
+class mqttClass 
+{
+    constructor()
+    {
+        this.config = new(require('../Configmanager/configmanager'))();
+        this.mqtt = require('async-mqtt');
+        this.mqttUrl = "mqtt://localhost";
+
+        this.mqttTopic1 = "alertTimeLimit";//->surpassed/under
+        this.mqttTopic2 = "alertTempLimit";//->surpassed/under
+        this.mqttClient = this.mqtt.connect(this.mqttUrl);
+        this.zwischenSpeicher = null;
+    }
+    
+    //Abonniert die zwei Topics
+    mqttClientSubscribeToTopic = (topic) => 
+    {
+        this.mqttClient.subscribe([this.mqttTopic1, this.mqttTopic2], {}, (error, granted) => 
+        {
+            if (granted !== null) 
+            {
+            console.log("Subscription erfolgreich erstellt. Topic = " + topic);
+            }
+            else 
+            {
+            console.error("Subscription konnte nicht erstellt werden.");
+            }
+        });
+    }
+    
+    //Funktion, welche beim erfolgreichen Verbinden mit dem MQTT Server abgearbeitet wird.
+    onConnect()
+    {
+        this.mqttClient.on("connect", async () => 
+        {
+            console.log('MQTT wurde erfolgreich verbunden. Adresse = ' + this.mqttUrl);
+            //Ausgewählte Topics abonnieren
+            await this.mqttClientSubscribeToTopic();
+            
+            //await mqttClient.subscribe(mqttTopic1);
+            //await mqttClient.subscribe(mqttTopic2);
+        });
+    }
+
+    //Funktion, welche bei einer neuen MQTT Nachricht in dem Subscription Topic ausgeführt wird.
+    mqttMessage()
+    {
+        this.mqttClient.on("message", (topic, message) => 
+        {
+            this.zwischenSpeicher = message.toString().trim().split('"').join("");
+            //console.log('Wert erhalten: ', zwischenSpeicher, ' on topic ', topic);
+            switch (topic) 
+            {
+                case 'alertTimeLimit':
+                    timeMessage = this.zwischenSpeicher;
+                    console.log('alertTimeLimit: ' + this.zwischenSpeicher);
+                    break;
+                case 'alertTempLimit':
+                    tempMessage = this.zwischenSpeicher;
+                    console.log('alertTempLimit: ' + this.zwischenSpeicher);
+                    break;
+            }
+            emailServiceObject.sendMailDecision();
+        });
+    }
+}
+
+
+let timeMessage = null;
+let tempMessage = null;
 const emailServiceObject = new EmailService();
+const client = new mqttClass();
+client.onConnect();
+client.mqttMessage();
 
-//MQTT inspiriert von Stefan Stumpf's Git
-const mqttClient = mqtt.connect(mqttUrl);
 
-//Abonniert die zwei Topics
-mqttClientSubscribeToTopic = (topic) => 
-{
-  mqttClient.subscribe([mqttTopic1, mqttTopic2], {}, (error, granted) => 
-  {
-    if (granted !== null) 
-    {
-      console.log("Subscription erfolgreich erstellt. Topic = " + topic);
-    }
-    else 
-    {
-      console.error("Subscription konnte nicht erstellt werden.");
-    }
-  });
-};
 
-//Funktion, welche beim erfolgreichen Verbinden mit dem MQTT Server abgearbeitet wird.
-mqttClient.on("connect", async () => 
-{
-  console.log('MQTT wurde erfolgreich verbunden. Adresse = ' + mqttUrl);
-
-  // Abonniere ausgewähltes Thema
-  await mqttClient.subscribe(mqttTopic1);
-  await mqttClient.subscribe(mqttTopic2);
-});
-
-let zwischenSpeicher = null; 
-//Funktion, welche bei einer neuen MQTT Nachricht in dem Subscription Topic ausgeführt wird.
-mqttClient.on("message", (topic, message) => {
-  zwischenSpeicher = message.toString().trim().split('"').join("");
-  //console.log('Wert erhalten: ', zwischenSpeicher, ' on topic ', topic);
-  switch (topic) 
-  {
-      case 'alertTimeLimit':
-          timeMessage = zwischenSpeicher;
-          console.log('alertTimeLimit: ' + zwischenSpeicher);
-          break;
-      case 'alertTempLimit':
-          tempMessage = zwischenSpeicher;
-          console.log('alertTempLimit: ' + zwischenSpeicher);
-          break;
-  }
-  emailServiceObject.sendMailDecision();
-});
+ 
