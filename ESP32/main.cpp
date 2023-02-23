c3f48ec98259a7e19ba3f15f933c449660c6e24c
Dennis Roth <dennisroth.4.2001@gmail.com>
2023-02-23 22:58:34 +0100

    Änderungen von Wlan konfigurationen auf Felix Handy Hotspot zudem Bautratenänderung

--- ESP32/main.cpp
+++ ESP32/main.cpp
@@ -14,12 +14,10 @@ DHT dht(4,DHT11);
 
 // Wlan und MQTT Client einrichten/Objekte davon anlegen 
 
-const char *ssid = "WLAN-P8UQS9";
-const char *password = "5463872899872204";
-const char *mqtt_server = "smartfridge.local"; //25531de9ef514270b301b8b4cc260c07.s2.eu.hivemq.cloud
-//const char* mqttUser = "den.roth.21";
-//const char* mqttPassword = "8_sXskxLH7rrKlj";
-const int mqtt_port = 8883;
+const char *ssid = "iPhone Felix";
+const char *password = "Felix234";
+const char *mqtt_server = "smartfridge.local";
+const int mqtt_port = 1883;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
@@ -27,54 +25,30 @@ PubSubClient client(espClient);
 //Parameter: keine 
 class TempSensor{
     public:
-
+      
       //Konstruktor baut Wlan verbindung auf und stellt eine Verbindung mit MQTT Broker her
       //Parameter: keine
       //return: kein 
       TempSensor(){
-        //Mit Wlan verbinden
         
-        WiFi.begin(ssid, password);
-        while (WiFi.status() != WL_CONNECTED)
-        {
-          delay(1000);
-          Serial.println("Connecting to WiFi...");
-        }
-        Serial.println("Connected to WiFi");
-
-        //Mit MQTT Broker verbinden 
-
-        client.setServer(mqtt_server, mqtt_port);
-
-        while (!client.connect("ESP"))
-        {
-          Serial.println("Failed to connect to MQTT broker. Trying again...");
-          delay(1000);
-        }
-        Serial.println("Connected to MQTT broker");
-
-        //Relevante Topics abonnieren 
-
-        client.subscribe("doorState");
       }
 
       String temperature(){
-        return String(dht.readTemperature())+".0";
+        return String(round(dht.readTemperature()))+".0";
       }
 
       //Sendet Temperaturmesswerte unter dem Topic tempOutside
       //Parameter: keine
       //return: kein
       void sendData(){
-        String tempValue = temperature();
+        String tempValue = temperature()+ ".0";
 
         //Konvertierung in chararray
 
-        char temp[3];
-        for (int i = 0; i < tempValue.length() - 3; i++)
-        {
-          temp[i] = tempValue[i];
-        }
+        char temp[5];
+        
+        tempValue.toCharArray(temp, 5);
+        Serial.println(temp);
         client.publish("tempOutside",temp);
       }
 
@@ -119,10 +93,39 @@ void onMessage(char * topic, uint8_t * payload, unsigned int length) {
   sensorOutside.processMessages(topic,payload,length);
 }
 
+void initializeWifi(){
+  //Mit Wlan verbinden
+        
+        WiFi.begin(ssid, password);
+        while (WiFi.status() != WL_CONNECTED)
+        {
+          delay(1000);
+          Serial.println("Connecting to WiFi...");
+        }
+        Serial.println("Connected to WiFi");
+}
+
+void initializeMQTT(){
+  //Mit MQTT Broker verbinden 
+
+        client.setServer(mqtt_server, mqtt_port);
+
+        while (!client.connect("ESP"))
+        {
+          Serial.println("Failed to connect to MQTT broker. Trying again...");
+          delay(1000);
+        }
+        Serial.println("Connected to MQTT broker");
+        client.subscribe("doorState");
+}
 //Arduino setup und loop funktionen 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  initializeWifi();
+  initializeMQTT();
+  
+  
   dht.begin(); //Starten Temperaturwerte zu lesen 
   pinMode(2, OUTPUT); // Setzt PIN 2 welches die Onbord LED ist als Output
 }
