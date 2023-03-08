08d3e8c7a2564e7620552fafe37040ea07621311
Dennis Roth <dennisroth.4.2001@gmail.com>
2023-03-08 21:54:03 +0100

    unnötige Komentare entfernt aus mosquitto.conf

--- VM/node.sh
+++ VM/node.sh
@@ -7,6 +7,10 @@ cd ..
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-
+# alle Packages installieren 
 
 npm install --all
+
+sudo apt-get install libcap2-bin
+
+sudo setcap cap_net_bind_service=+ep /usr/local/bin/node
